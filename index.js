5fa103f9d792eb0b306845fea5fa90c4d589f6d7
magyarblip <magyarblip@gmail.com>
2025-03-13 11:32:22 -0400

    index.js: be generally more explicit about properties and variables

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 import { encode, decode } from '@digitalbazaar/cborld';
 import { JsonLdDocumentLoader } from 'jsonld-document-loader';
 
-const contextObject = {"@context": {"ex": "https://example.com/my-context/v1#", "type": "@type", "Note": "ex:Note", "summary": "ex:summary", "content": "ex:content"}};
+const CONTEXT_URL = 'https://example.com/my-context/v1'; 
+const CONTEXT = {
+  '@context': {
+    ex: 'https://example.com/my-context/v1#', 
+    type: '@type', 
+    Example: 'ex:Example', 
+    label: 'ex:label', 
+    content: 'ex:content'
+  }
+};
 
 const loader = new JsonLdDocumentLoader();
-loader.addStatic('https://example.com/my-context/v1', contextObject)
-const documentLoader = loader.build();
+loader.addStatic(CONTEXT_URL, CONTEXT)
+const myLoader = loader.build();
 
-const jsonldDocument = {
+const myDocument = {
   '@context': 'https://example.com/my-context/v1',
   type: 'Note',
-  summary: 'CBOR-LD',
+  label: 'CBOR-LD',
   content: 'CBOR-LD is awesome!'
 };
 
 // encode a JSON-LD Javascript object into CBOR-LD bytes
-const cborldBytes = await encode({ jsonldDocument, documentLoader });
-console.log(cborldBytes);
+const myCborld = await encode({ jsonldDocument: myDocument, documentLoader: myLoader });
+console.log(myCborld);
 
 //decode to get back what we put in
-const roundtripJsonLdDocument = await decode({ cborldBytes, documentLoader })
+const roundtripJsonLdDocument = await decode({ cborldBytes: myCborld, documentLoader: myLoader })
 console.log(roundtripJsonLdDocument);
 
 const reencodedOptions = {
   jsonldDocument: roundtripJsonLdDocument,
-  documentLoader: documentLoader
+  documentLoader: myLoader
 }
 // rountripping fails due to missing '#' in namespace
 const reencodedCborldBytes = await encode(reencodedOptions);

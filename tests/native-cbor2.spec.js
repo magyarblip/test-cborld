d5e57e5daa439d95f5f9333db865869fa4c8bee9
magyarblip <magyarblip@gmail.com>
2025-03-14 19:24:29 -0400

    isolate cbor2 native test

--- tests/native-cbor2.spec.js
+++ tests/native-cbor2.spec.js
@@ -1,8 +1,6 @@
-import {expect} from 'chai';
-//   import {default as chaiBytes} from 'chai-bytes';
-//   chai.use(chaiBytes);
-
+import { expect } from 'chai';
 import { encode, decode } from '@digitalbazaar/cborld';
+import { encode as encode2, decode as decode2 } from 'cbor2';
 import { JsonLdDocumentLoader } from 'jsonld-document-loader';
 
 const CONTEXT_URL = 'https://example.com/my-context/v1';
@@ -16,25 +14,14 @@ const CONTEXT = {
     }
 };
 
-const ALT_CONTEXT_URL = 'https://example.com/my-context/v2';
-const ALT_CONTEXT = {
-    '@context': {
-        ex: 'https://example.com/my-context#',
-        type2: '@type',
-        Example2: 'ex:Example',
-        label2: 'ex:label',
-        content2: 'ex:content'
-    }
-};
-
-
 function _loadContext(ctx_url, ctx) {
     const loader = new JsonLdDocumentLoader();
     loader.addStatic(ctx_url, ctx)
     return loader.build();
 }
 
-describe('cborld encode', () => {
+
+describe('cborld decode', () => {
     it('should load into the new context',
         async () => {
             const myDocument = {
@@ -50,21 +37,10 @@ describe('cborld encode', () => {
 
             // Problem is this is bound to the original context URL, rather than its namespace
             // How do i inspect the CBOR data in a context neutral way
-            // const output = await decode2(myCborldBytes);
-            // console.log(output);
+            const output = await decode2(myCborldBytes);
+            console.log(output);
+            console.log(output.contents.get(0));
 
-
-            const altLoader = _loadContext(CONTEXT_URL, ALT_CONTEXT);
-            const roundtripJsonLdDocument = await decode({ cborldBytes: myCborldBytes, documentLoader: altLoader })
-            // console.log(roundtripJsonLdDocument);
-
-            const expectedDocument = {
-                '@context': CONTEXT_URL,
-                type2: 'Example2',
-                label2: 'CBOR-LD',
-                content2: 'CBOR-LD is awesome!'
-            };
-
-            expect(roundtripJsonLdDocument).to.deep.equal(expectedDocument);
-        });
+        }
+    )
 });
